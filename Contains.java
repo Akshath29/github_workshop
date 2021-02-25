66783638a79c940ce701ea9fbc8f8e90691d3ed8
nephaes <s2056170@ed.ac.uk>
2021-02-25 13:46:12 +0000

    I added contains

--- Contains.java
+++ Contains.java
@@ -1,8 +1,19 @@
 package github_workshop;
 public class Contains
 {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World");
+    public static int main(String[] args) {
+        int[] num = (1,2,3,4,5);
+        int toFind = 3;
+        boolean fount = true;
+
+        for (n:num) {
+            if (n= toFind) {
+                found = true;
+                break
+            }
+        }
+
+        if (found) print (toFind + "is Found.");
+        else print (toFind + "is not found.");
     }
 }
